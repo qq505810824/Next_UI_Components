44736ac0ea077a2e0823ee9b50f43b92d856e859
聪 <505810824@qq.com>
2025-09-04 09:26:39 +0800

    add tag input

--- dist/components/TagInput/index.d.ts
+++ dist/components/TagInput/index.d.ts
@@ -1,2 +1,3 @@
 export { default as TagInput } from './TagInput';
+export type { TagInputProps } from './TagInput';
 //# sourceMappingURL=index.d.ts.map
--- src/components/TagInput/index.ts
+++ src/components/TagInput/index.ts
@@ -1,1 +1,3 @@
 export { default as TagInput } from './TagInput';
+export type { TagInputProps } from './TagInput';
+
